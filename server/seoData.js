f1c7b0a0757476b190f684617831594e1677665d
John Livingston <jmlivingston@gmail.com>
2017-08-29 10:31:26 -0700

    Update based on latest

--- server/seoData.js
+++ server/seoData.js
@@ -3,36 +3,43 @@ const seoData = {
   paths: {
     default: {
       'title': 'Home',
-      'description': 'About walking dogs, running, and learning Japanese',
-      'google-site-verification': '0d7dcd2b-6110-4a91-afea-c9754c4f772a',
-      'keywords': 'dogs, running, Japanese',
-      'msvalidate.01': '886d0992-f21d-4eb4-9eec-e278d01f782a',
-      'twitter:description': 'About walking dogs, running, and learning Japanese',
-      'twitter:image': 'https://www.example.com/logo.jpg',
-      'twitter:title': 'This is an Awesome Site',
-      'twitter:url': 'https://www.example.com',
-      'article:author': 'John Doe',
-      'article:publisher': 'John Doe LLC',
-      'fb:app_id': 'dfe41b88-2307-43f0-9af6-15ff7b18f940',
-      'og:description': 'About walking dogs, running, and learning Japanese',
-      'og:image': 'https://www.example.com/logo.jpg',
-      'og:site_name': 'Awesome Site',
-      'og:title': 'This is an Awesome Site',
-      'og:type': 'website',
-      'og:url': 'https://www.example.com'
+      'meta': {
+        'description': { value: 'Home', type: 'name' },
+        'google-site-verification': { value: '0d7dcd2b-6110-4a91-afea-c9754c4f772a', type: 'name' },
+        'keywords': { value: 'dogs, running, Japanese', type: 'name' },
+        'msvalidate.01': { value: '886d0992-f21d-4eb4-9eec-e278d01f782a', type: 'name' },
+        'twitter:description': { value: 'About walking dogs, running, and learning Japanese', type: 'name' },
+        'twitter:image': { value: 'https://www.example.com/logo.jpg', type: 'name' },
+        'twitter:title': { value: 'This is an Awesome Site', type: 'name' },
+        'twitter:url': { value: 'https://www.example.com', type: 'name' },
+        'article:author': { value: 'John Doe', type: 'property' },
+        'article:publisher': { value: 'John Doe LLC', type: 'property' },
+        'fb:app_id': { value: 'dfe41b88-2307-43f0-9af6-15ff7b18f940', type: 'property' },
+        'og:description': { value: 'About walking dogs, running, and learning Japanese', type: 'property' },
+        'og:image': { value: 'https://www.example.com/logo.jpg', type: 'property' },
+        'og:site_name': { value: 'Awesome Site', type: 'property' },
+        'og:title': { value: 'This is an Awesome Site', type: 'property' },
+        'og:type': { value: 'website', type: 'property' },
+        'og:url': { value: 'https://www.example.com', type: 'property' }
+      }
     },
     '/todo/1': {
       'title': 'Todo 1',
-      'description': 'This is my first todo',
-      'twitter:url': 'https://www.example.com',
+      'meta': {
+        'description': { value: 'This is my first todo', type: 'name' }
+      }
     },
     '/todo/2': {
       'title': 'Todo 2',
-      'description': 'This is my second todo'
+      'meta': {
+        'description': { value: 'This is my second todo', type: 'name' }
+      }
     },
     '/todo/3': {
       'title': 'Todo 3',
-      'description': 'This is my third todo'
+      'meta': {
+        'description': { value: 'This is my third todo', type: 'name' }
+      }
     }
   }
 }
--- server/seoViewEngine.js
+++ server/seoViewEngine.js
@@ -3,21 +3,28 @@ const seoData = require('./seoData.js')
 
 function getSeoFromFile (url) {
   return seoData.paths[url]
-    ? { ...seoData.paths.default, ...seoData.paths[url] }
-    : seoData.paths.default
+    ? {
+      title: seoData.titlePrefix + (seoData.paths[url].title || seoData.paths.default.title),
+      meta: { ...seoData.paths.default.meta, ...seoData.paths[url].meta }
+    }
+    : {
+      title: seoData.titlePrefix + seoData.paths.default.title,
+      meta: { ...seoData.paths.default.meta }
+    }
 }
 
 function seoViewEngine (filePath, options, callback) {
   fs.readFile(filePath, 'utf8', function (error, fileContents) {
     if (error) return callback(error)
+    // Note: You should use a database or routing class instead
     const seo = getSeoFromFile(options.url)
-    for (const prop in seo) {
-      if (prop === 'title') {
-        fileContents = fileContents.replace('__' + prop + '__', seoData.titlePrefix + seo[prop])
-      } else {
-        fileContents = fileContents.replace('__' + prop + '__', seo[prop])
-      }
-    }
+    const separator = '\n\t' // optional
+    let metaTags = Object.entries(seo.meta).map(([key, value]) => {
+      return `<meta ${value.type}="${key}" content="${value.value}">`
+    })
+    metaTags.unshift(`<title>${seo.title}</title>`)
+    const metaTagsFormatted = separator + metaTags.join(separator) + separator
+    fileContents = fileContents.replace('<head>', '<head>' + metaTagsFormatted)
     callback(null, fileContents)
   })
 }
